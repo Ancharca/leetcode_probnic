6a08acb14a99630572a7ed4382ea595da76f048f
Ancharcaaaaaa <A.l.e.n.a.D.VRN@gmail.com>
2025-04-30 21:41:36 +0300

    problem with duplicate values

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void mail(String args[]){
-        System.out.println("Goodbye America ebat");
+    public static void main(String args[]){
+        int[] arr = {1, 2, 3, 4, 6};
+
+        if (Solution.containsDuplicate(arr)){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
---
+++ Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++){
+
+        }
+        return false;
+    }
+}
