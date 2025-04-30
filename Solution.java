8d27c1d558a50b22fc7e7c2be48846599cbd34c0
Ancharcaaaaaa <A.l.e.n.a.D.VRN@gmail.com>
2025-04-30 21:56:45 +0300

    problem with duplicate values

--- Solution.java
+++ Solution.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+
+import java.util.Arrays;
 public class Solution {
     public static boolean containsDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            if (seen.contains(num)) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
             }
-            seen.add(num);
         }
         return false;
     }
